3b201d7aa1a5ffd361cb2923cd67569e21718465
suihongwei <18845795735@163.com>
2021-06-03 17:35:47 +0800

    delete: ClassNameExtension

--- add_log_for_method_plugin/src/main/java/com/shawn/add_log_for_method_plugin/ClassNameExtension.kt
+++
@@ -1,7 +0,0 @@
-package com.shawn.add_log_for_method_plugin
-
-import org.gradle.api.Project
-
-class ClassNameExtension constructor(project: Project) {
-    var classNames: ArrayList<String>? = null
-}
