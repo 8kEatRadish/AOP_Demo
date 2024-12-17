629ea0dc73d0089d79d64aa2edfcea1ebb48133d
8kEatRadish <18845795735@163.com>
2024-12-18 02:07:15 +0800

    feat:添加测试代码

--- add_log_for_method_plugin/src/main/java/com/shawn/add_log_for_method_plugin/AddLogInitMethodVisitor.kt
+++ add_log_for_method_plugin/src/main/java/com/shawn/add_log_for_method_plugin/AddLogInitMethodVisitor.kt
@@ -12,57 +12,15 @@ class AddLogInitMethodVisitor(methodVisitor: MethodVisitor) :
         mv.visitLdcInsn("AddLogMethodPlugin")
         mv.visitTypeInsn(Opcodes.NEW, "java/lang/StringBuilder")
         mv.visitInsn(Opcodes.DUP)
-        mv.visitMethodInsn(
-            Opcodes.INVOKESPECIAL,
-            "java/lang/StringBuilder",
-            "<init>",
-            "()V",
-            false
-        )
+        mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false)
         mv.visitLdcInsn("into ")
-        mv.visitMethodInsn(
-            Opcodes.INVOKEVIRTUAL,
-            "java/lang/StringBuilder",
-            "append",
-            "(Ljava/lang/String;)Ljava/lang/StringBuilder;",
-            false
-        )
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false)
         mv.visitVarInsn(Opcodes.ALOAD, 0)
-        mv.visitMethodInsn(
-            Opcodes.INVOKEVIRTUAL,
-            "java/lang/Object",
-            "getClass",
-            "()Ljava/lang/Class;",
-            false
-        )
-        mv.visitMethodInsn(
-            Opcodes.INVOKEVIRTUAL,
-            "java/lang/Class",
-            "getSimpleName",
-            "()Ljava/lang/String;",
-            false
-        )
-        mv.visitMethodInsn(
-            Opcodes.INVOKEVIRTUAL,
-            "java/lang/StringBuilder",
-            "append",
-            "(Ljava/lang/String;)Ljava/lang/StringBuilder;",
-            false
-        )
-        mv.visitMethodInsn(
-            Opcodes.INVOKEVIRTUAL,
-            "java/lang/StringBuilder",
-            "toString",
-            "()Ljava/lang/String;",
-            false
-        )
-        mv.visitMethodInsn(
-            Opcodes.INVOKESTATIC,
-            "com/shawn/krouter/uitl/UtilsKt",
-            "MyLogD",
-            "(Ljava/lang/String;Ljava/lang/String;)V",
-            false
-        )
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/Object", "getClass", "()Ljava/lang/Class;", false)
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/Class", "getSimpleName", "()Ljava/lang/String;", false)
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false)
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false)
+        mv.visitMethodInsn(Opcodes.INVOKESTATIC, "com/shawn/krouter/uitl/UtilsKt", "MyLogD", "(Ljava/lang/String;Ljava/lang/String;)V", false)
     }
 
 
--- add_log_for_method_plugin/src/main/java/com/shawn/add_log_for_method_plugin/AddLogMethodVisitor.kt
+++ add_log_for_method_plugin/src/main/java/com/shawn/add_log_for_method_plugin/AddLogMethodVisitor.kt
@@ -12,61 +12,19 @@ class AddLogMethodVisitor(methodVisitor: MethodVisitor) :
         mv.visitLdcInsn("AddLogMethodPlugin")
         mv.visitTypeInsn(Opcodes.NEW, "java/lang/StringBuilder")
         mv.visitInsn(Opcodes.DUP)
-        mv.visitMethodInsn(
-            Opcodes.INVOKESPECIAL,
-            "java/lang/StringBuilder",
-            "<init>",
-            "()V",
-            false
-        )
+        mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false)
         mv.visitLdcInsn("into ")
-        mv.visitMethodInsn(
-            Opcodes.INVOKEVIRTUAL,
-            "java/lang/StringBuilder",
-            "append",
-            "(Ljava/lang/String;)Ljava/lang/StringBuilder;",
-            false
-        )
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false)
         mv.visitTypeInsn(Opcodes.NEW, "java/lang/Exception")
         mv.visitInsn(Opcodes.DUP)
         mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Exception", "<init>", "()V", false)
-        mv.visitMethodInsn(
-            Opcodes.INVOKEVIRTUAL,
-            "java/lang/Exception",
-            "getStackTrace",
-            "()[Ljava/lang/StackTraceElement;",
-            false
-        )
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/Exception", "getStackTrace", "()[Ljava/lang/StackTraceElement;", false)
         mv.visitInsn(Opcodes.ICONST_0)
         mv.visitInsn(Opcodes.AALOAD)
-        mv.visitMethodInsn(
-            Opcodes.INVOKEVIRTUAL,
-            "java/lang/StackTraceElement",
-            "getMethodName",
-            "()Ljava/lang/String;",
-            false
-        )
-        mv.visitMethodInsn(
-            Opcodes.INVOKEVIRTUAL,
-            "java/lang/StringBuilder",
-            "append",
-            "(Ljava/lang/String;)Ljava/lang/StringBuilder;",
-            false
-        )
-        mv.visitMethodInsn(
-            Opcodes.INVOKEVIRTUAL,
-            "java/lang/StringBuilder",
-            "toString",
-            "()Ljava/lang/String;",
-            false
-        )
-        mv.visitMethodInsn(
-            Opcodes.INVOKESTATIC,
-            "com/shawn/krouter/uitl/UtilsKt",
-            "MyLogD",
-            "(Ljava/lang/String;Ljava/lang/String;)V",
-            false
-        )
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StackTraceElement", "getMethodName", "()Ljava/lang/String;", false)
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false)
+        mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false)
+        mv.visitMethodInsn(Opcodes.INVOKESTATIC, "com/shawn/krouter/uitl/UtilsKt", "MyLogD", "(Ljava/lang/String;Ljava/lang/String;)V", false)
     }
 
 
